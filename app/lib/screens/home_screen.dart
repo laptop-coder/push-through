2c47f7a6196ab6f4b0ac7675f03d6533bdb0cb51
laptop-coder <coderlaptop@gmail.com>
2026-06-22 00:41:51 +0300

    feat: style home screen, add delete animation

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:push_through/models/workout.dart';
 import 'package:push_through/services/workout_service.dart';
 import 'package:push_through/screens/workout_screen.dart';
 import 'package:push_through/utils/format_datetime.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -15,6 +16,77 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool _loading = true;
   List<Workout> _workouts = [];
+  GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+
+  void _deleteWorkout(int index, Workout workout) async {
+    final removedWorkout = _workouts[index];
+    _workouts.removeAt(index);
+    _listKey.currentState!.removeItem(
+      index,
+      (context, animation) => SlideTransition(
+        position: Tween<Offset>(
+          begin: Offset.zero,
+          end: Offset(1.0, 0.0),
+        ).animate(CurvedAnimation(parent: animation, curve: Curves.easeIn)),
+        child: FadeTransition(
+          opacity: animation,
+          child: _buildSlidableItem(removedWorkout, index),
+        ),
+      ),
+      duration: Duration(milliseconds: 300),
+    );
+    await WorkoutService.delete(workout.id);
+    if (_workouts.isEmpty) {
+      _loadWorkouts();
+    }
+  }
+
+  Widget _buildSlidableItem(Workout workout, int index) {
+    final child = Slidable(
+      key: Key(workout.id.toString()),
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        extentRatio: 0.3,
+        children: [
+          SlidableAction(
+            onPressed: (context) => _deleteWorkout(index, workout),
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Удалить',
+            borderRadius: BorderRadius.circular(32),
+          ),
+        ],
+      ),
+      child: Material(
+        clipBehavior: Clip.antiAlias,
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(index == 0 ? 12 : 2),
+          bottom: Radius.circular(index == _workouts.length - 1 ? 12 : 2),
+        ),
+        child: ListTile(
+          visualDensity: VisualDensity(vertical: 2),
+          title: Text(FormatDatetime.formatDate(workout.createdAt)),
+          subtitle: Text(
+            FormatDatetime.formatDateRelativeToToday(
+              DateTime.parse(workout.createdAt.replaceAll(' ', 'T')),
+            ),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => WorkoutScreen(workoutId: workout.id),
+              ),
+            ).then((_) => _loadWorkouts());
+          },
+        ),
+      ),
+    );
+
+    return Padding(padding: EdgeInsets.only(bottom: 4), child: child);
+  }
 
   @override
   void initState() {
@@ -26,6 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
     final workouts = await WorkoutService.getAll();
     setState(() {
       _workouts = workouts;
+      _listKey = GlobalKey<AnimatedListState>();
       _loading = false;
     });
   }
@@ -38,39 +111,27 @@ class _HomeScreenState extends State<HomeScreen> {
     ).then((_) => _loadWorkouts());
   }
 
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surfaceContainerLow,
+        title: const Text('Дожми! Жим лёжа'),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surfaceContainerLow,
       body: _loading
           ? Center(child: CircularProgressIndicator())
           : _workouts.isEmpty
           ? Center(child: Text('Нет тренировок'))
-          : ListView.builder(
-              itemCount: _workouts.length,
-              itemBuilder: (context, index) {
-                final workout = _workouts[index];
-                return Card(
-                  child: ListTile(
-                    title: Text(FormatDatetime.formatDate(workout.createdAt)),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => WorkoutScreen(workoutId: workout.id),
-                        ),
-                      ).then((_) => _loadWorkouts());
-                    },
-                  ),
-                );
-              },
+          : Padding(
+              padding: EdgeInsets.only(left: 12, right: 12, top: 12),
+              child: AnimatedList(
+                key: _listKey,
+                initialItemCount: _workouts.length,
+                itemBuilder: (context, index, animation) {
+                  return _buildSlidableItem(_workouts[index], index);
+                },
+              ),
             ),
       floatingActionButton: FloatingActionButton(
         onPressed: _createWorkout,
